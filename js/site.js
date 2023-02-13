cbd20ed355deff200d054ee3a38b69c502dbb0c2
RedAngelRedux <sammy@sammynava.com>
2023-02-12 21:29:37 -0500

    Write and Test Code
    Successfully wrote the code to  satisfy the exercises requirement.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,37 @@
-// Call HelloWorld
-function HelloWorld() {
-    alert("Hello World");
+// CONTROLLER FUNCTION
+function getValues() {
+
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+    
+    displayString(revString);
+
+}
+
+// LOGIC FUNCTION
+function reverseString(userString) {
+
+    let strArray = [];
+
+    // reverse the string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {  
+        
+        strArray += userString[index];   
+    }
+
+    return strArray;
+
+}
+
+// VIEW FUNCTION
+function displayString(rString) {
+
+    // write to the page
+    document.getElementById("msg").innerHTML = `On my world, you would say that like this: ${rString}`;
+
+    // show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
